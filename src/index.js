f224b6e25aac28e8011d4008d653bbec34080048
Daniel Anton Mata <daniel.mata@sykes.com>
2018-08-10 00:19:58 +0800

    Turned on MobX strict mode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import 'semantic-ui-css/semantic.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure } from 'mobx';
 
 import Example from './Example';
+
+// MobX configuration
+configure({ enforceActions: true });
 
 ReactDOM.render(<Example />, document.getElementById('root'));
